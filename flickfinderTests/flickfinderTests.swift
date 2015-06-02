6c8e0e5fc8fbcedfb66a98de4980b01b0b0a9a97
Eric N. Winn <enwinn@gmail.com>
2015-06-01 23:11:23 -0400

    Initial Commit

---
+++ flickfinderTests/flickfinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  flickfinderTests.swift
+//  flickfinderTests
+//
+//  Created by Eric Winn on 6/1/15.
+//  Copyright (c) 2015 Eric N. Winn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class flickfinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
